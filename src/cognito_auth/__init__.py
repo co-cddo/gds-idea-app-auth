b7a9a6e5747bcb57f152e531167c5a0bb91697bc
David Gillespie <david.gillespie@digital.cabinet-office.gov.uk>
2025-10-08 13:55:42 +0100

    Add some logging to authoriser

--- src/cognito_auth/__init__.py
+++ src/cognito_auth/__init__.py
@@ -1,6 +1,12 @@
+import logging
+
 from .authoriser import Authoriser
 from .exceptions import ExpiredTokenError, InvalidTokenError, MissingTokenError
 from .user import User
+
+# Add NullHandler to prevent "No handler found" warnings
+# Consuming applications should configure their own logging handlers
+logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __all__ = [
     "User",
